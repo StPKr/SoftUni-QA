243f5cb2d26af482663f22357199e9e300541581
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-11 12:27:04 +0300

    .

--- Programming Fundamentals for QA/16 Methods - Lab/07. Greater of Two Values/Program.cs
+++ Programming Fundamentals for QA/16 Methods - Lab/07. Greater of Two Values/Program.cs
@@ -7,7 +7,38 @@
 
             static int FindGreaterNumber(int num1, int num2)
             {
-                throw new NotImplementedException();
+                if (num1 > num2)
+                {
+                    return num1;
+                }
+                else
+                {
+                    return num2;
+                }
+            }
+
+            static char FindGreaterChar(char ch1, char ch2)
+            {
+                if (ch1 > ch2)
+                {
+                    return ch1;
+                }
+                else
+                {
+                    return ch2;
+                }
+            }
+
+            static string  FindGreaterString(string? str1, string? str2)
+            {
+                if (str1.CompareTo(str2) > 0)
+                {
+                    return str1;
+                }
+                else
+                {
+                    return str2;
+                }
             }
             string valueType = Console.ReadLine();
 
@@ -16,20 +47,24 @@
                 int num1 = int.Parse(Console.ReadLine());
                 int num2 = int.Parse(Console.ReadLine());
 
-                Console.WriteLine(FindGreaterNumber(num1, num2));
+                Console.WriteLine(FindGreaterNumber(num1, num2)); 
             }
             else if (valueType == "char") 
             {
                 char ch1 = char.Parse(Console.ReadLine());
                 char ch2 = char.Parse(Console.ReadLine());
+
+                Console.WriteLine(FindGreaterChar(ch1, ch2));
             }
             else if (valueType == "string")
             {
                 string str1 = Console.ReadLine();
                 string str2 = Console.ReadLine();
-            }
-        }
 
-        
+                Console.WriteLine(FindGreaterString(str1, str2));
+            }
+
+        }
+              
     }
 }
