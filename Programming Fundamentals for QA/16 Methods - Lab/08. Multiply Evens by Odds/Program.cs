7db625c49e97b70a94647c2b416549e88508fe55
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-11 12:49:48 +0300

    Task 9

--- Programming Fundamentals for QA/16 Methods - Lab/08. Multiply Evens by Odds/Program.cs
+++ Programming Fundamentals for QA/16 Methods - Lab/08. Multiply Evens by Odds/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+int output = GetMultipleOfEvenAndOdds(number);
+
+Console.WriteLine(output);
+int GetMultipleOfEvenAndOdds(int number)
+{
+    int sumEven = GetSumOfEvenDigits(number);
+    int sumOdd = GetSumOfOddDigits(number);
+
+    int result = sumEven * sumOdd;
+    return result;
+}
+
+int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 == 0)
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
+
+int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 != 0)
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
---
+++ Programming Fundamentals for QA/16 Methods - Lab/09. Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿string product = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+double result = CalculatePrice(product, quantity);
+
+Console.WriteLine($"{result:F2}");
+
+static double CalculatePrice(string product, int quantity)
+{
+    double result = 0;
+
+    if (product == "coffee")
+    {
+        result = quantity * 1.50;
+    }
+    else if (product == "water")
+    {
+        result = quantity * 1.00;
+    }
+    else if (product == "coke")
+    {
+        result = quantity * 1.40;
+    }
+    else if (product == "snacks")
+    {
+        result = quantity * 2.00;
+    }
+
+    return result;
+}
