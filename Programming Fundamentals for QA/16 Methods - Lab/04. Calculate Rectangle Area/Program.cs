b11ae564321d2c924fd97c8efb74ef753f236738
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-11 12:09:40 +0300

    Daily commit

--- Programming Fundamentals for QA/16 Methods - Lab/04. Calculate Rectangle Area/Program.cs
+++ Programming Fundamentals for QA/16 Methods - Lab/04. Calculate Rectangle Area/Program.cs
@@ -4,15 +4,14 @@
     {
         static void Main(string[] args)
         {
+            static int Area(int w, int l)
+            {
+                return w * l;
+            }
             int width = int.Parse(Console.ReadLine());
             int length = int.Parse(Console.ReadLine());
-
-            static void Area(int width, int length)
-            {
-                int area = width * length;
-                Console.WriteLine(area);
-            }
-            Area(width, length);
+            int area = Area(width, length);
+            Console.WriteLine(area);
         }
     }
 }
