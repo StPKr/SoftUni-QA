50c1236da606a52799dbbed1fa72f9897495ffa0
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-11 12:26:14 +0300

    Task 7

--- Programming Fundamentals for QA/16 Methods - Lab/07. Greater of Two Values/Program.cs
+++ Programming Fundamentals for QA/16 Methods - Lab/07. Greater of Two Values/Program.cs
@@ -4,8 +4,32 @@
     {
         static void Main(string[] args)
         {
-            string type = Console.ReadLine();
 
+            static int FindGreaterNumber(int num1, int num2)
+            {
+                throw new NotImplementedException();
+            }
+            string valueType = Console.ReadLine();
+
+            if (valueType == "int")
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(FindGreaterNumber(num1, num2));
+            }
+            else if (valueType == "char") 
+            {
+                char ch1 = char.Parse(Console.ReadLine());
+                char ch2 = char.Parse(Console.ReadLine());
+            }
+            else if (valueType == "string")
+            {
+                string str1 = Console.ReadLine();
+                string str2 = Console.ReadLine();
+            }
         }
+
+        
     }
 }
