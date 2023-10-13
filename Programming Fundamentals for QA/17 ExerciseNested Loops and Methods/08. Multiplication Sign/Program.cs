4a85e6aa1420ecd9ecd01166773b4c159e34644c
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-13 15:12:21 +0300

    Task 8

---
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/08. Multiplication Sign/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+static string SignChecker(int a, int b, int c)
+{
+    if (a == 0 || b == 0 || c == 0)
+    {
+        return "zero";
+    }
+    else if (((a > 0) && (b > 0) && (c > 0))
+        || ((a < 0) && (b > 0) && (c < 0))
+        || ((a < 0) && (b < 0) && (c > 0))
+        || ((a > 0) && (b < 0) && (c < 0)))
+    {
+        return "positive";
+    }
+    else
+    {
+        return "negative";
+    }
+}
+Console.WriteLine(SignChecker(a, b, c));
