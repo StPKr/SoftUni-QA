b54400193172cf21eba50bc0d9b154278e87c401
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-13 11:22:34 +0300

    Task 3

--- Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/03. Unique PIN Codes/Program.cs
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/03. Unique PIN Codes/Program.cs
@@ -1,7 +1,6 @@
 ﻿int max1 = int.Parse(Console.ReadLine());
 int max2  = int.Parse(Console.ReadLine());
 int max3 = int.Parse(Console.ReadLine());
-string output = "";
 for (int i = 2; i <= max1; i+=2)
 {
     for (int j = 1; j <= max2; j++)
@@ -10,8 +9,7 @@ for (int i = 2; i <= max1; i+=2)
         {
             if (j == 2 || j == 3 || j == 5 || j == 7)
             {
-                output = $"{i}{j}{k}";
-                Console.WriteLine(output);
+                Console.WriteLine($"{i}{j}{k}");
             }
         }
     }
