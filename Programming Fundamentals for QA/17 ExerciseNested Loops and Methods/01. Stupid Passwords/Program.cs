49948274805589cd80b73669fe214996af642a03
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-13 11:16:50 +0300

    Task 1

--- Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/01. Stupid Passwords/Program.cs
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/01. Stupid Passwords/Program.cs
@@ -1,11 +1,11 @@
 ﻿int n = int.Parse(Console.ReadLine());
-string output = "";
+
 for  (int i = 2; i <= n; i += 2)
 {
     for (int j = 1; j <= n; j += 2)
     {
-        output += $"{i}{j}{i * j} ";
+        Console.Write($"{i}{j}{i * j} ");
     }
 }
-Console.WriteLine(output);
+
 
