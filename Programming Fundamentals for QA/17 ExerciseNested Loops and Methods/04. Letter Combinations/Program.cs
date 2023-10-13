1e49addef70003bde9a70637210e2c2b733756a5
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-13 11:55:16 +0300

    Task 4

--- Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/04. Letter Combinations/Program.cs
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/04. Letter Combinations/Program.cs
@@ -1,17 +1,21 @@
 ﻿char start = char.Parse(Console.ReadLine());
 char end = char.Parse(Console.ReadLine());
-char excluded  = char.Parse(Console.ReadLine());
-string output = "";
+char excluded = char.Parse(Console.ReadLine());
+int count = 0;
 for (char i = start; i <= end; i++)
 {
     for (char j = start; j <= end; j++)
     {
         for (char k = start; k <= end; k++)
         {
-           if (i != excluded &&  j != excluded &&  k != excluded){
-            Console.Write($"{i}{j}{k} ");
+            if (i != excluded && j != excluded && k != excluded)
+            {
+                Console.Write($"{i}{j}{k} ");
+                count++;
             }
 
         }
     }
 }
+Console.WriteLine();
+Console.WriteLine(count);
