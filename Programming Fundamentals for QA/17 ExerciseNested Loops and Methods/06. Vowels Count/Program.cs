aa287a3eddcc5fa311e9c5864effeabca0bb012a
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-13 14:59:08 +0300

    Task 7

--- Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/06. Vowels Count/Program.cs
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/06. Vowels Count/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int VowelsCount(string text)
+{
+    int count = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        char c = text[i];
+        if (c == 'A' || c == 'a' ||
+            c == 'E' || c == 'e' ||
+            c == 'O' || c == 'o' ||
+            c == 'U' || c == 'u' ||
+            c == 'I' || c == 'i') 
+        { 
+            count++;
+        }
+    }
+    return(count);
+}
+string text = Console.ReadLine();
+Console.WriteLine(VowelsCount(text));
---
+++ Programming Fundamentals for QA/17 ExerciseNested Loops and Methods/07. Factorial Division/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b =  int.Parse(Console.ReadLine());
+Console.WriteLine(FactorialDivision(a, b));
+static double FactorialDivision(int a, int b)
+{
+    double factorialA = 1;
+    double factorialB = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        factorialA *= i; 
+    }
+    for (int j = 1; j <= b; j++)
+    {
+        factorialB *= j;
+    }
+    return factorialA / factorialB;
+}
