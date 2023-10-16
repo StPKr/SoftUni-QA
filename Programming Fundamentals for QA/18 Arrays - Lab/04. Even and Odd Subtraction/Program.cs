2741e930501ebbbaf13ded31824f49779c37ddea
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-16 22:53:09 +0300

    Task 4

--- Programming Fundamentals for QA/18 Arrays - Lab/04. Even and Odd Subtraction/Program.cs
+++ Programming Fundamentals for QA/18 Arrays - Lab/04. Even and Odd Subtraction/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+int evenSum = 0;
+int oddSum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    int element = numbers[i];
+    if (element %2 == 0)
+    {
+        evenSum += element;
+    } else
+    {
+        oddSum += element;
+    }
+}
+Console.WriteLine(evenSum - oddSum);
