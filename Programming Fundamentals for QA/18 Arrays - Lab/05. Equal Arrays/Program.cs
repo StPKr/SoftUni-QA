6c3db0d32d6e33fe66e198aa5cc8a3d6bb57de7d
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-16 22:58:12 +0300

    Task 5

---
+++ Programming Fundamentals for QA/18 Arrays - Lab/05. Equal Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+int[] array1 = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+int[] array2 = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+bool check = true;
+for (int i = 0; i < array1.Length; i++)
+{
+    int element1 = array1[i];
+    int element2 = array2[i];
+
+    if (element1 != element2)
+    {
+        check = false;
+        break;
+    }
+}
+    
+if (check)
+    {
+        Console.WriteLine("Arrays are identical.");
+    }
+    else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
