360d75219202fa1bbaa2a936c4110166d9837d08
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-16 22:50:25 +0300

    Task 2

---
+++ Programming Fundamentals for QA/18 Arrays - Lab/02. Sum an Array/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine(sum);
---
+++ Programming Fundamentals for QA/18 Arrays - Lab/03. Reverse an Array/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] numbers =  new int[n];
+for (int i = 0; i < n; i++)
+{
+    numbers[i]= int.Parse(Console.ReadLine());
+}
+for (int j = n - 1; j >= 0; j--)
+{
+    Console.Write(numbers[j] + " ");
+    }
+    
