8d2f00237e66b2f6ac34b6f16d181678bb75671d
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-16 22:05:18 +0300

    Task 1

--- Programming Fundamentals for QA/18 Arrays - Lab/01. Day of Week/Program.cs
+++ Programming Fundamentals for QA/18 Arrays - Lab/01. Day of Week/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 && day <= 7)
+    Console.WriteLine(days[day - 1]);
+else
+    Console.WriteLine("Invalid day!");
