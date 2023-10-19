a8efb7c15e8db92e193aad470dcd671b0f7027e3
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-19 19:51:39 +0300

    Task 5

--- Programming Fundamentals for QA/19 Lists - Lab/04. List Manipulation Basics/Program.cs
+++ Programming Fundamentals for QA/19 Lists - Lab/04. List Manipulation Basics/Program.cs
@@ -1,3 +1,27 @@
 ﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 string command = Console.ReadLine();
-while (command !)
+while (command != "end")
+{
+    string[] commandParts = command.Split(" ");
+    string commandName = commandParts[0];
+    int element = int.Parse(commandParts[1]);
+    // int element = int.Parse(command.Split(" ")[1]; - оптимален начин;
+    switch (commandName)
+    {
+        case "Add":
+            integers.Add(element);
+            break;
+        case "Remove":
+            integers.Remove(element);
+            break;
+        case "RemoveAt":
+            integers.RemoveAt(element);
+            break;
+        case "Insert":
+            int element2 = int.Parse(commandParts[2]);
+            integers.Insert(element2, element);
+            break;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", integers));
