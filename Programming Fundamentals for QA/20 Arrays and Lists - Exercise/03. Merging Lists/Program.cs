86d033a744c4b7f16a0365bd98222bf9148fa178
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-19 23:59:30 +0300

    Task 3

---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/03. Merging Lists/Program.cs
@@ -0,0 +1,6 @@
+﻿List<int> list1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> list2 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int elem1 = list1[2];
+int elem2 = list2[2];
+Console.WriteLine(elem1);
+Console.WriteLine(elem2);
