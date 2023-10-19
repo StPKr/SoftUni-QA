ef15b94388a1ea0ae422fc7015f9514cb2dd53b2
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-19 17:32:56 +0300

    Lists
    Creating folder and Task 1

---
+++ Programming Fundamentals for QA/19 Lists - Lab/01. Change List/Program.cs
@@ -0,0 +1,29 @@
+﻿string values = Console.ReadLine();
+List<int> list = values.Split(" ").Select(int.Parse).ToList();
+string commands = Console.ReadLine();
+List<string> elements = commands.Split(" ").ToList();
+while (elements[0] != "end")
+{
+    switch (elements[0])
+    {
+        case "Delete":
+            while (list.Contains(int.Parse(elements[1]))){ 
+            foreach (int a in list)
+            {
+                if (a == int.Parse(elements[1]))
+                {
+                    list.Remove(a);
+                }
+            }
+            }
+            break;
+        case "Insert":
+            list.Insert(int.Parse(elements[1]), int.Parse(elements[2]));
+            break;
+
+
+    }
+    commands = Console.ReadLine();
+    elements = commands.Split(" ").ToList();
+}
+Console.WriteLine(string.Join(" ", list));
