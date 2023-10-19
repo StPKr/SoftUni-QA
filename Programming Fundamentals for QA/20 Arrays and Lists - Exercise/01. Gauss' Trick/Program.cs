9bf30c5d122d0d0367c25b08699efac73a576365
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-19 23:48:15 +0300

    Task 2

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
@@ -1,24 +1,15 @@
 ﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 List<int> sumArray = new List<int>();
 int sum = 0;
-for (int i = 0; i <= integers.Count()/2; i++)
+for (int i = 0; i < integers.Count()/2; i++)
 {
     int reverseIndex = integers.Count() - 1 - i;
-    if (integers.Count() % 2 == 0)
-    {
     sum = integers[i] + integers[reverseIndex];
-    }
-    else
-    {
-        if (i == reverseIndex - 1)
-        {
-            sum = integers[i];
-        }
-        else
-        {
-            sum = integers[i] + integers[reverseIndex];
-        }
-    }
     sumArray.Add(sum);
+}
+if (integers.Count() % 2 != 0)
+{
+    int lastElement = integers[integers.Count() / 2];
+    sumArray.Add(lastElement);
 }
 Console.WriteLine(string.Join(" ", sumArray));
---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/02. Condense Array to Number/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+while (integers.Count() > 1)
+{
+    List<int> outputList = new List<int>();
+    for (int i = 0; i < integers.Count() - 1; i++)
+    {
+
+        outputList.Add(integers[i] + integers[i + 1]);
+    }
+    integers = outputList;
+}
+Console.WriteLine(string.Join("", integers));
