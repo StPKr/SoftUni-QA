6c7a1daba453f4c3ae64f76b25de46366382983e
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 00:31:26 +0300

    Task 4

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/04. Sum Adjacent Equal Numbers/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/04. Sum Adjacent Equal Numbers/Program.cs
@@ -1,4 +1,4 @@
-﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+﻿/*List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 bool check = true;
 while (check && integers.Count > 1)
 {
@@ -6,9 +6,8 @@ while (check && integers.Count > 1)
     {
         if (integers[i] == integers[i + 1])
             {
-                integers.Insert(i, integers[i] * 2);
-                integers.RemoveAt(i + 1);
-                integers.RemoveAt(i + 1);
+            integers[i] *= 2;
+            integers.RemoveAt(i + 1);
             check = true;
             break;
             }
@@ -19,10 +18,16 @@ while (check && integers.Count > 1)
 
     }
 }
-Console.WriteLine(string.Join(" ", integers));
+Console.WriteLine(string.Join(" ", integers)); - каруцарски*/
 
-/*List<int> test = new List<int>() { 1, 2, 3, 4 };
-test.Insert(1, 5);
-test.RemoveAt(2);
-test.RemoveAt(2);
-Console.WriteLine(string.Join(" ",test));*/
+List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+for (int i = 0; i < integers.Count() - 1; i++)
+{
+    if (integers[i] == integers[i + 1])
+    {
+        integers[i] *= 2;
+        integers.RemoveAt(i + 1);
+        i = -1;
+    }
+}
+Console.WriteLine(string.Join(" ", integers));
