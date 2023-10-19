9a586f831dffdcad7df13b19d0ef31ac77e882b1
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 00:23:52 +0300

    Task 4

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/03. Merging Lists/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/03. Merging Lists/Program.cs
@@ -1,6 +1,15 @@
 ﻿List<int> list1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 List<int> list2 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-int elem1 = list1[2];
-int elem2 = list2[2];
-Console.WriteLine(elem1);
-Console.WriteLine(elem2);
+List<int> output = new List<int>();
+for (int i = 0; i < Math.Max(list1.Count(), list2.Count()); i++)
+{
+    if (i < list1.Count)
+    {
+        output.Add(list1[i]);
+    }
+    if (i < list2.Count)
+    {
+        output.Add(list2[i]);
+    }
+}
+Console.WriteLine(string.Join(" ", output));
---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/04. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+bool check = true;
+while (check && integers.Count > 1)
+{
+    for (int i = 0; i < integers.Count() - 1; i++)
+    {
+        if (integers[i] == integers[i + 1])
+            {
+                integers.Insert(i, integers[i] * 2);
+                integers.RemoveAt(i + 1);
+                integers.RemoveAt(i + 1);
+            check = true;
+            break;
+            }
+        else
+        {
+            check = false;
+        }
+
+    }
+}
+Console.WriteLine(string.Join(" ", integers));
+
+/*List<int> test = new List<int>() { 1, 2, 3, 4 };
+test.Insert(1, 5);
+test.RemoveAt(2);
+test.RemoveAt(2);
+Console.WriteLine(string.Join(" ",test));*/
