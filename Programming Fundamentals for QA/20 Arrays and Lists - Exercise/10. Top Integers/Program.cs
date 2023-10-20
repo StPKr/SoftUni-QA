e35bc7ba41ae2bc208d8b9c9cdecb5d006b9219e
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 14:55:04 +0300

    Closing commit

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/10. Top Integers/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/10. Top Integers/Program.cs
@@ -5,7 +5,7 @@ for (int i = 0; i < integers.Count - 1; i++)
     bool check = true;
     for (int j = i + 1; j < integers.Count; j++)
     {
-        if (integers[i] < integers[j])
+        if (integers[i] <= integers[j])
         {
             check = false;
             break;
