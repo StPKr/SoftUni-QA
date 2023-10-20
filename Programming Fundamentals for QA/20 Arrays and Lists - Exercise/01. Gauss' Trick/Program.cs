18ff84e808a07eb0db586eb32890ac51ff80dc7a
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 11:02:05 +0300

    Cleanup changes

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
@@ -1,7 +1,7 @@
 ﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
 List<int> sumArray = new List<int>();
 int sum = 0;
-for (int i = 0; i < integers.Count()/2; i++)
+for (int i = 0; i < integers.Count() / 2; i++)
 {
     int reverseIndex = integers.Count() - 1 - i;
     sum = integers[i] + integers[reverseIndex];
