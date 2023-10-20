21c30e2ec3345c98ec28fd53357a4f83da2f3e36
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 14:45:34 +0300

    Task 9

---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/09. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int maxSequence = 0;
+int lastIndex = 0;
+for (int i = 0; i < integers.Count(); i++)
+{
+    int currentSequence = 1;
+    for (int j = i + 1; j < integers.Count(); j++)
+    {
+        if (integers[i] == integers[j])
+        {
+            currentSequence++;
+        }
+        else
+        {
+            break;
+        }
+        if (maxSequence < currentSequence)
+        {
+            maxSequence = currentSequence;
+            lastIndex = j;
+        }
+    }
+}
+string result = "";
+for (int k = lastIndex; k > lastIndex - maxSequence; k--)
+{
+    result += integers[k] + " ";
+}
+Console.WriteLine(result);
