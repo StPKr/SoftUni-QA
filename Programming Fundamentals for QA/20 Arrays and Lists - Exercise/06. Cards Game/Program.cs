60a8ac7788e1fd31f3e399df09755d6b09be6a54
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 14:24:11 +0300

    Task 6

--- Programming Fundamentals for QA/20 Arrays and Lists - Exercise/06. Cards Game/Program.cs
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/06. Cards Game/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> deck1 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> deck2 = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+while (deck1.Count() > 0 && deck2.Count() > 0)
+{
+    if (deck1[0] > deck2[0])
+    {
+        deck1.Add(deck1[0]);
+        deck1.Add(deck2[0]);
+
+    }
+    else if (deck1[0] < deck2[0])
+    {
+        deck2.Add(deck2[0]);
+        deck2.Add(deck1[0]);
+    }
+    deck1.Remove(deck1[0]);
+    deck2.Remove(deck2[0]);
+}
+int sum = 0;
+if (deck1.Count() > deck2.Count())
+{
+    sum = deck1.Sum();
+    Console.WriteLine("First player wins! Sum: " + sum);
+}
+else
+{
+    sum = deck2.Sum();
+    Console.WriteLine("Second player wins! Sum: " + sum);
+}
---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/08. Array Rotation/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int rotations = int.Parse(Console.ReadLine());
+for (int i = 0; i < rotations; i++)
+{
+    int firstElement = integers[0];
+    integers.RemoveAt(0);
+    integers.Add(firstElement);
+}
+Console.WriteLine(string.Join(" ", integers));
