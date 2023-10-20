55494187e6b0943096981c4d490450e59929ee55
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 11:36:36 +0300

    Task 5

---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int[] commands = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int bombNumber = commands[0];
+int power = commands[1];
+while (integers.Contains(bombNumber))
+{
+    int position = integers.IndexOf(bombNumber);
+    int numbersToRemove = power * 2 + 1;
+    int startIndex = position - power;
+
+    if (startIndex < 0)
+    {
+        startIndex = 0;
+    }
+    integers.RemoveRange(startIndex, numbersToRemove);
+    }
+    //integers.RemoveRange(startIndex, endIndex);
+}
+int sum = integers.Sum();
+Console.WriteLine(sum);
