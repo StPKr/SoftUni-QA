8e06cd2692b0b8af6be2e25d6cc578b234b42afb
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-10-20 14:54:00 +0300

    Top 10

---
+++ Programming Fundamentals for QA/20 Arrays and Lists - Exercise/10. Top Integers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> topIntegers = new List<int>();
+for (int i = 0; i < integers.Count - 1; i++)
+{
+    bool check = true;
+    for (int j = i + 1; j < integers.Count; j++)
+    {
+        if (integers[i] < integers[j])
+        {
+            check = false;
+            break;
+        }
+    }
+    if (check)
+    {
+        topIntegers.Add(integers[i]);
+    }
+}
+topIntegers.Add(integers[integers.Count - 1]);
+Console.WriteLine(string.Join(" ", topIntegers));
