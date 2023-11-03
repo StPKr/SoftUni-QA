0ad8ccf57738e8dcf044a6fea44e2bd03f50b2ff
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-03 17:42:38 +0200

    Test 7

--- Programming Fundamentals for QA/23 Exercises Unit Testing Methods, Arrays and Lists/TestApp.UnitTests/PatternTests.cs
+++ Programming Fundamentals for QA/23 Exercises Unit Testing Methods, Arrays and Lists/TestApp.UnitTests/PatternTests.cs
@@ -14,23 +14,45 @@ public class PatternTests
         int[]? inputArray = null;
 
         // Act + Assert
+        Assert.That(() => Pattern.SortInPattern(inputArray), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_SortInPattern_SortsIntArrayInPattern_SortsCorrectly()
     {
-        // TODO: finish test
+        // Arrange
+        int[] numbers = new int[] { 1, 2, 3 };
+
+        // Act
+
+        int[] result = Pattern.SortInPattern(numbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] {1,3,2}));
     }
 
     [Test]
     public void Test_SortInPattern_EmptyArray_ReturnsEmptyArray()
     {
-        // TODO: finish test
+        // Arrange
+        int[] numbers = new int[] {};
+
+        // Act
+
+        int[] result = Pattern.SortInPattern(numbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(new int[] { }));
     }
 
     [Test]
     public void Test_SortInPattern_SingleElementArray_ReturnsSameArray()
     {
-        // TODO: finish test
+        // Arrange
+        int[] numbers = new int[] { 3 };
+
+        // Act
+
+        int[] result = Pattern.SortInPattern(numbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(new[] { 3 }));
     }
 }
