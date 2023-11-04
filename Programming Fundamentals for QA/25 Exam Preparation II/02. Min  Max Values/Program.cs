f8d7bd51a15b5f938bcb247cab0f5b678f93e307
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 12:34:53 +0200

    Task 2

--- Programming Fundamentals for QA/25 Exam Preparation II/02. Min  Max Values/Program.cs
+++ Programming Fundamentals for QA/25 Exam Preparation II/02. Min  Max Values/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = Console.ReadLine()
+   .Split(" ")
+   .Select(int.Parse)
+   .ToArray();
+int n = int.Parse(Console.ReadLine());
+
+List<int> list = new List<int>();
+for (int i = 0; i < n; i++)
+{
+    list.Add(array[i]);
+}
+list.Sort();
+Console.WriteLine(list[n -1]);
+Console.WriteLine(list[0]);
