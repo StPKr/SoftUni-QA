76376397f87b66bbe50720c57557a4a0b5907a17
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 11:35:48 +0200

    Task 2

---
+++ Programming Fundamentals for QA/24 Exam Preparation I/02. Middle Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+if (array.Length > 0)
+{
+    int index1 = array.Length / 2 - 1;
+    int index2 = array.Length / 2;
+
+    double a = array[index1];
+    double b = array[index2];
+    double result = (a + b) / 2;
+    Console.WriteLine(result.ToString("F2"));
+}
+else
+{
+    Console.WriteLine(0);
+}
