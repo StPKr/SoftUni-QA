ef64cddb777d822e3da401c1f983d8dede4de445
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 12:27:11 +0200

    Task 1

---
+++ Programming Fundamentals for QA/25 Exam Preparation II/01. Magic Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool isThereNoMatchingNumbers = true;
+for (int i = 1; i <= n; i++)
+{
+    int currentNum = i;
+    int digitSum = 0;
+    bool isAllDigitsPrime = true;
+
+    while (currentNum > 0)
+    {
+        int digit = currentNum % 10;
+        currentNum = currentNum / 10;
+
+        bool isDigitPrime = digit == 2 || digit == 3 || digit == 5 || digit == 7;
+        if (isDigitPrime)
+        {
+            digitSum += digit;
+        }
+        else
+        {
+            isAllDigitsPrime = false;
+            break;
+        }
+    }
+    if (isAllDigitsPrime && digitSum %2 == 0)
+    {
+        Console.Write(i + " ");
+        isThereNoMatchingNumbers = false;
+    }
+}
+if (isThereNoMatchingNumbers)
+{
+    Console.WriteLine("no");
+}
