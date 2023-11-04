822ef65b497b1778886797822c9eacafaba7d534
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 11:20:09 +0200

    Task 1

---
+++ Programming Fundamentals for QA/24 Exam Preparation I/01. Sum Factorial Even Digits/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    int n = int.Parse(input[i].ToString());
+    if (n % 2 == 0)
+    {
+        int placeholder = 1;
+        for (int j = n; j > 0; j--)
+        {
+            placeholder *= j;
+        }
+        sum += placeholder;
+    }
+}
+Console.WriteLine(sum);
