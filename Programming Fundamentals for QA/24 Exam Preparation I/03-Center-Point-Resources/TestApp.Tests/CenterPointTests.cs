856982f9abc1a61aab23bde86b63edbe9a1171e3
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 11:49:59 +0200

    Task 3

--- Programming Fundamentals for QA/24 Exam Preparation I/03-Center-Point-Resources/TestApp.Tests/CenterPointTests.cs
+++ Programming Fundamentals for QA/24 Exam Preparation I/03-Center-Point-Resources/TestApp.Tests/CenterPointTests.cs
@@ -8,30 +8,65 @@ public class CenterPointTests
     [Test]
     public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        double firstPointX = 0;
+        double firstPointY = 0;
+        double secondPointX = 1;
+        double secondPointY = 1;
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+         
+        Assert.AreEqual("(0, 0)", result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        double firstPointX = 1;
+        double firstPointY = 1;
+        double secondPointX = 0;
+        double secondPointY = 0;
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        Assert.AreEqual("(0, 0)", result);
     }
 
     [Test]
     public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        double firstPointX = 1;
+        double firstPointY = 1;
+        double secondPointX = 1;
+        double secondPointY = 1;
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        Assert.AreEqual("(1, 1)", result);
     }
 
     [Test]
     public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        double firstPointX = -2;
+        double firstPointY = -3;
+        double secondPointX = 2;
+        double secondPointY = 3;
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        Assert.AreEqual("(-2, -3)", result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        double firstPointX = 2;
+        double firstPointY = 1;
+        double secondPointX = -2;
+        double secondPointY = -1;
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        Assert.AreEqual("(-2, -1)", result);
     }
 }
