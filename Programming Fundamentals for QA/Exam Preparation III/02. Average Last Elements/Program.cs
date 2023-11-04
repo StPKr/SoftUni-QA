8208da25e4573a8a43f550ddbca1852c4ee821e8
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 12:50:14 +0200

    Task 2

--- Programming Fundamentals for QA/Exam Preparation III/02. Average Last Elements/Program.cs
+++ Programming Fundamentals for QA/Exam Preparation III/02. Average Last Elements/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = Console.ReadLine()
+  .Split(" ")
+  .Select(int.Parse)
+  .ToArray();
+int n = int.Parse(Console.ReadLine());
+int firstElement = array.Length - 1;
+int lastElement = array.Length - n;
+List<double> list = new List<double>();
+for (int i = firstElement; i >= lastElement; i--)
+{
+    list.Add(array[i]);
+}
+double sum = list.Sum() / n;
+Console.WriteLine(sum.ToString("F2"));
