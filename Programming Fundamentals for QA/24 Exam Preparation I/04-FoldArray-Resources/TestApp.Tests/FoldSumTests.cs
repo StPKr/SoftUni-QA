edd8088ccdbe685863bc9208df937e4cc867894f
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-04 12:01:00 +0200

    Task 4

--- Programming Fundamentals for QA/24 Exam Preparation I/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
+++ Programming Fundamentals for QA/24 Exam Preparation I/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -5,13 +5,15 @@ namespace TestApp.Tests;
 
 public class FoldSumTests
 {
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
+    [TestCase(new int[] { 1, 2, 3, 4 }, "3 7")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] { 2, 2, 2, 2 }, "4 4")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, "7 7 7 19 19 19")]
+    [TestCase(new int[] { }, "")]
     public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
     {
-        // TODO: implement the test and finish the test cases
+        string result = FoldSum.FoldArray(arr);
+
+        Assert.AreEqual(expected, result);
     }
 }
