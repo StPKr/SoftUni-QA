8835fc270e2f5d6ed6a56e6558602d89ddcc4e39
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 09:26:21 +0200

    Task 3

---
+++ Programming Fundamentals for QA/Exam/Resources/TestApp.Tests/CharacterRangeTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class CharacterRangeTests
+{
+    [Test]
+    public void Test_GetRange_WithAAndBInOrder_ReturnsEmptyString()
+    {
+        char first = 'A';
+        char second = 'B';
+
+        string result = CharacterRange.GetRange(first, second);
+
+        Assert.That(result, Is.EqualTo(""));
+    }
+
+    [Test]
+    public void Test_GetRange_WithBAndAInOrder_ReturnsEmptyString()
+    {
+        char first = 'B';
+        char second = 'A';
+
+        string result = CharacterRange.GetRange(first, second);
+
+        Assert.That(result, Is.EqualTo(""));
+    }
+
+    [Test]
+    public void Test_GetRange_WithAAndCInOrder_ReturnsB()
+    {
+        char first = 'A';
+        char second = 'C';
+
+        string result = CharacterRange.GetRange(first, second);
+
+        Assert.That(result, Is.EqualTo("B"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithDAndGInOrder_Returns_E_F()
+    {
+        char first = 'D';
+        char second = 'G';
+
+        string result = CharacterRange.GetRange(first, second);
+
+        Assert.That(result, Is.EqualTo("E F"));
+    }
+
+    [Test]
+    public void Test_GetRange_WithXAndZInOrder_Returns_Y()
+    {
+        char first = 'X';
+        char second = 'Z';
+
+        string result = CharacterRange.GetRange(first, second);
+        
+        Assert.That(result, Is.EqualTo("Y"));
+    }
+}
---
+++ Programming Fundamentals for QA/Exam/Resources/TestApp/CharacterRange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestApp;
+
+public class CharacterRange
+{
+    public static string GetRange(char a, char b)
+    {
+        int chOne = Math.Min(a, b);
+        int chTwo = Math.Max(a, b);
+
+        string result = string.Empty;
+        for (int i = chOne + 1; i < chTwo; i++)
+        {
+            result += $"{(char)i} ";
+        }
+
+        return result.Trim();
+    }
+}
