11a809e81bc1a9d4ca4271745a5306b37117dfd2
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 08:57:23 +0200

    Task 3

---
+++ Programming Fundamentals for QA/25 Exam Preparation II/03-Pascal-Triangle-Resources/TestApp.Tests/PascalTriangleTests.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class PascalTriangleTests
+{
+    //[TestCase()]
+    //[TestCase()]
+    //[TestCase()]
+    //[TestCase()]
+    //[TestCase()]
+    public void Test_PrintTriangle_ShouldReturnCorrectString(int n, string expected)
+    {
+        // TODO: implement the test and finish the test cases
+    }
+}
---
+++ Programming Fundamentals for QA/25 Exam Preparation II/04-LIS-Resources/TestApp.Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        int[] arr = null;
+
+        Assert.That(() => LongestIncreasingSubsequence.GetLis(arr), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        int[] arr = Array.Empty<int>();
+
+        string output = LongestIncreasingSubsequence.GetLis(arr);
+
+        Assert.That(output, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        // TODO: implement the test
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        // TODO: implement the test
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        // TODO: implement the test
+    }
+}
