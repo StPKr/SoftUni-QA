7e53c94944808be01c98e91f14799e9e4f7b6fa9
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 09:43:52 +0200

    Task 4

---
+++ Programming Fundamentals for QA/Exam/Resources 4/TestApp.Tests/BalancedBracketsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        string[] input = new string[] { };
+
+        bool output = BalancedBrackets.IsBalanced(input);
+
+        Assert.That(output, Is.True);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        string[] input = new string[] { "(", ")", "(", ")" };
+
+        bool output = BalancedBrackets.IsBalanced(input);
+
+        Assert.That(output, Is.True);
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(", "(", ")" };
+
+        bool output = BalancedBrackets.IsBalanced(input);
+
+        Assert.That(output, Is.False);
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { "(", ")", "(", ")", ")" };
+
+        bool output = BalancedBrackets.IsBalanced(input);
+
+        Assert.That(output, Is.False);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = new string[] { ")", "(" };
+
+        bool output = BalancedBrackets.IsBalanced(input);
+
+        Assert.That(output, Is.False);
+    }
+}
---
+++ Programming Fundamentals for QA/Exam/Resources 4/TestApp/BalancedBrackets.cs
@@ -0,0 +1,28 @@
+﻿namespace TestApp;
+
+public class BalancedBrackets
+{
+    public static bool IsBalanced(string[] input)
+    {
+        int balance = 0;
+
+        foreach (string symbol in input)
+        {
+            if (symbol == "(")
+            {
+                balance++;
+            }
+            else if (symbol == ")")
+            {
+                balance--;
+            }
+
+            if (balance < 0)
+            {
+                return false;
+            }
+        }
+
+        return balance == 0;
+    }
+}
