330e2f3f43f8b6a15eb27c5aa5df10cc55986855
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 09:03:15 +0200

    Task 1

---
+++ Programming Fundamentals for QA/Exam/1/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+double sum = 0;
+for (int i = 0; i < n; i++)
+{
+    double grade = double.Parse(Console.ReadLine());
+    sum += grade;
+}
+double average = sum / n;
+Console.WriteLine(average.ToString("F2"));
