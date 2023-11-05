e09de812d5a22ab85d4c734658a7ed4f23991b64
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-05 09:15:55 +0200

    Task 2

--- Programming Fundamentals for QA/Exam/ConsoleApp1/Program.cs
+++ Programming Fundamentals for QA/Exam/ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> integers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    integers.Insert(0, integers[integers.Count - 1]);
+    integers.RemoveAt(integers.Count - 1);
+
+}
+Console.WriteLine(string.Join(", ", integers));
