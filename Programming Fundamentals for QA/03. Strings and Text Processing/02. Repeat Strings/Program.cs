85bdf77880274a474691f3183a6b4704ea2d8014
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-07 21:07:34 +0200

    Task 2

---
+++ Programming Fundamentals for QA/03. Strings and Text Processing/02. Repeat Strings/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] inputArray = Console.ReadLine().Split(" ").ToArray();
+string commonElements = "";
+for (int i = 0; i < inputArray.Length; i++)
+{
+    for (int j = 0; j < inputArray[i].Length; j++)
+        commonElements += inputArray[i];
+}
+Console.WriteLine(commonElements);
