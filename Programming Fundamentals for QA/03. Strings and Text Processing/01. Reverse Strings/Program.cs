8a22b4e51e63119a2e607c0ed399cc14335a1645
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-07 20:55:36 +0200

    Task 1

---
+++ Programming Fundamentals for QA/03. Strings and Text Processing/01. Reverse Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿string command = Console.ReadLine();
+while (command != "end")
+{
+    char[] list = command.ToCharArray();
+    Array.Reverse(list);
+    string reversed = new string(list);
+    Console.WriteLine(command + " = " + reversed);
+    command = Console.ReadLine();
+}
