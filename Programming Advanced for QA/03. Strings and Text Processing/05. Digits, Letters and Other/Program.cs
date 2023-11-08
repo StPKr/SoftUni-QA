e84bdf7348e1b45fcb93334743e4902bbee030aa
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-08 14:43:14 +0200

    Task 5

---
+++ Programming Advanced for QA/03. Strings and Text Processing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,20 @@
+﻿string test = Console.ReadLine();
+string digits = "";
+string letters = "";
+string rest = "";
+for (int i = 0; i < test.Length; i++)
+{
+    if (char.IsDigit(test[i]))
+    {
+        digits += test[i];
+    } else if (char.IsLetter(test[i]))
+    {
+        letters += test[i];
+    } else
+    {
+        rest += test[i];
+    }
+}
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(rest);
