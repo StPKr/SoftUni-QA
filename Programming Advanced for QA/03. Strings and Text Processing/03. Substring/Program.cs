df5662a01072fcfb59f1d9842382a87756b035de
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-08 13:42:30 +0200

    Таск 3 & 4

---
+++ Programming Advanced for QA/03. Strings and Text Processing/03. Substring/Program.cs
@@ -0,0 +1,10 @@
+﻿string test = Console.ReadLine();
+string phrase = Console.ReadLine();
+while (phrase.Contains(test))
+{
+    int indexOfTest = phrase.IndexOf(test);
+    phrase = phrase.Remove(indexOfTest, test.Length);
+
+    // phrase = phrase.Replace(test, "");
+}
+Console.WriteLine(phrase);
---
+++ Programming Advanced for QA/03. Strings and Text Processing/04. Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] bannedWords = Console.ReadLine().Split(", ").ToArray();
+string text = Console.ReadLine();
+foreach (string word in bannedWords)
+{
+    string replacemeentPhrase = "";
+        for (int i = 0; i < word.Length; i++)
+    {
+        replacemeentPhrase += "*";
+    }
+    text = text.Replace(word, replacemeentPhrase);
+}
+Console.WriteLine(text);
