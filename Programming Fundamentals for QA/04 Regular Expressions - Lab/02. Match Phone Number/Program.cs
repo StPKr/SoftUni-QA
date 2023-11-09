aa9cb22289e08d771a01ef1c53c6e8cfe52b6a8e
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-09 23:42:10 +0200

    Task 3

--- Programming Fundamentals for QA/04 Regular Expressions - Lab/02. Match Phone Number/Program.cs
+++ Programming Fundamentals for QA/04 Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"\+359([\ \-])2\1\d{3}\1\d{4}\b";
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+Console.WriteLine(string.Join(", ", matches));
---
+++ Programming Fundamentals for QA/04 Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"\b(?<day>\d{2})(?<sep>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<sep>(?<year>\d{4})\b";
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+foreach (Match match in matches)
+{
+    Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+}
