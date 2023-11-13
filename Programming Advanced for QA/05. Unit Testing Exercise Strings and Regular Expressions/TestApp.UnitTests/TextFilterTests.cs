481e473bf0b073f5c9c7e720b617895dce7ec5c0
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-13 22:49:16 +0200

    Task 3

--- Programming Advanced for QA/05. Unit Testing Exercise Strings and Regular Expressions/TestApp.UnitTests/TextFilterTests.cs
+++ Programming Advanced for QA/05. Unit Testing Exercise Strings and Regular Expressions/TestApp.UnitTests/TextFilterTests.cs
@@ -11,28 +11,52 @@ public class TextFilterTests
     public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
     {
         // Arrange
+        string text = "The quick brown fox jumps over the lazy dog";
+        string[] banned = new string[] { "bear" };
 
         // Act
-
+        string result = TextFilter.Filter(banned, text);
         // Assert
-        //Assert.That(result, Is.EqualTo(text));
+        Assert.That(result, Is.EqualTo(text));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "The quick brown fox jumps over the lazy dog";
+        string[] banned = new string[] { "quick" };
+        string expected = "The ***** brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
     {
-        // TODO: finish the test
+        string text = "The quick brown fox jumps over the lazy dog";
+        string[] banned = Array.Empty<string>();
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+        // Assert
+        Assert.That(result, Is.EqualTo(text));
     }
 
     [Test]
     public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
     {
-        // TODO: finish the test
+        // Arrange
+        string text = "The quick brown fox jumps over the lazy dog";
+        string[] banned = new string[] { "quick brown", "dog" };
+        string expected = "The *********** fox jumps over the lazy ***";
+
+        // Act
+        string result = TextFilter.Filter(banned, text);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
