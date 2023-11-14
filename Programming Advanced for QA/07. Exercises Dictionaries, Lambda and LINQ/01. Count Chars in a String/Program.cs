3424fd6cb7da64a42102d0058fc3052556d63b90
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-14 20:42:01 +0200

    Task 1

---
+++ Programming Advanced for QA/07. Exercises Dictionaries, Lambda and LINQ/01. Count Chars in a String/Program.cs
@@ -0,0 +1,22 @@
+﻿string words = Console.ReadLine();
+Dictionary<char, int> charCount = new();
+for (int i = 0; i < words.Length; i++)
+{
+    char ch = words[i];
+    if ( ch == ' ')
+    {
+        continue;
+    }
+     if (charCount.ContainsKey(ch))
+    {
+        charCount[ch] += 1;
+    }
+    else
+    {
+        charCount.Add(ch, 1);
+    }
+}
+foreach (KeyValuePair<char, int> pair in charCount)
+{
+        Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
