fa9a248c1e9d5961d59ff81892e211b0ce4ba158
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-21 22:18:45 +0200

    Task 3

---
+++ Programming Advanced for QA/09. Objects and Classes/03. Store Boxes/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _03._Store_Boxes
+{
+    public class Item
+    {
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+
+    public class Box
+    {
+        public Box(string serialNumber, Item item, int itemQuantity)
+        {
+            SerialNumber = serialNumber;
+            Item = item;
+            ItemQuantity = itemQuantity;
+            PriceForABox = item.Price * itemQuantity;
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceForABox { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Box> boxes = new List<Box>();
+
+            while (command != "end")
+            {
+                string[] data = command.Split(" ");
+                string serialNumber = data[0];
+                string itemName = data[1];
+                int itemQuantity = int.Parse(data[2]);
+                decimal itemPrice = decimal.Parse(data[3]);
+
+                Item currentItem = new Item(itemName, itemPrice);
+                Box currentBox = new Box(serialNumber, currentItem, itemQuantity);
+
+                boxes.Add(currentBox);
+
+                command = Console.ReadLine();
+            }
+
+            boxes = boxes.OrderByDescending(x => x.PriceForABox).ToList();
+
+            foreach (Box box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:F2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceForABox:F2}");
+            }
+        }
+    }
+}
