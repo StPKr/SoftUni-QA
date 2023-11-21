12caa35b636b80fa722820d578583b6989155a2a
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-21 22:04:01 +0200

    Task 2

--- Programming Advanced for QA/09. Objects and Classes/00. Demo1/Dog.cs
+++ Programming Advanced for QA/09. Objects and Classes/00. Demo1/Dog.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace _00._Demo1
 {
-    internal class Dog
+    public class Dog
     {
+        public Dog()
+        {
+            Name = "Default Name!";
+
+        }
+
+        public Dog (string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+            Age = 1;
+        }
+
+        public Dog(string name, string breed, int age)
+        {
+            Name = name;
+            Breed = breed;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+    public string Breed { get; set; }
+
+    public int Age { get; set; }
+
+    public void Bark()
+    {
+        Console.WriteLine($"{Name}: Bau Bau");
     }
+}
 }
--- Programming Advanced for QA/09. Objects and Classes/00. Demo1/Program.cs
+++ Programming Advanced for QA/09. Objects and Classes/00. Demo1/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace _00._Demo1
+﻿using _00._Demo1;
+
+Dog puppy = new Dog()
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+    Name = "Sparky",
+    Breed = "Corgi",
+    Age = 3
+};
+
+Dog friend = new Dog();
+friend.Name = "Ivan";
+friend.Breed = "Unknown";
+friend.Age = 3;
+
+Dog dog = new Dog();
+
+Dog dog2 = new Dog("John", "Shiba Inu");
+
+Dog dog3 = new Dog("Bibi", "Golden Retriever", 5);
+
+
+Console.WriteLine(puppy.Name);
+Console.WriteLine(puppy.Age);
+
+Console.WriteLine(dog.Name);
+Console.WriteLine(dog.Age);
+
+Console.WriteLine(dog2.Name);
+Console.WriteLine(dog2.Age);
+
+Console.WriteLine(dog3.Name);
+Console.WriteLine(dog3.Age);
