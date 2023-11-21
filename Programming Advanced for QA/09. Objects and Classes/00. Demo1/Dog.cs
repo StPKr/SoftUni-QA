4fc35efe60f9b909982d7b1c2d29d2bf89b5a813
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-21 17:28:16 +0200

    Task 1

---
+++ Programming Advanced for QA/09. Objects and Classes/00. Demo1/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00._Demo1
+{
+    internal class Dog
+    {
+    }
+}
