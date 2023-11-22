76d78417783722c87607356dbf526c3db2e93a91
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-22 22:24:01 +0200

    Task 1 & 2

---
+++ Programming Advanced for QA/10. Defining Classes - Lab/CarManufacturer/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - distance*this.FuelConsumption > 0) //this. can be skipped
+            {
+                FuelQuantity -= distance*this.FuelConsumption;
+            } 
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ Programming Advanced for QA/10. Defining Classes - Lab/CarManufacturer/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car vwMK3 = new Car();
+            vwMK3.Make = "VW";
+            vwMK3.Model = "MK3";
+            vwMK3.Year = 1992;
+            vwMK3.FuelQuantity = 200;
+            vwMK3.FuelConsumption = 200;
+            vwMK3.Drive(2000);
+            Console.WriteLine(vwMK3.WhoAmI());
+        }
+    }
+}
