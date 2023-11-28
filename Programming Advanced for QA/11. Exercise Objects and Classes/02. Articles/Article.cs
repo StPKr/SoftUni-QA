309395948b6ccc6a89803e77a180338818b3fb0d
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-11-29 00:09:40 +0200

    Task 2

---
+++ Programming Advanced for QA/11. Exercise Objects and Classes/02. Articles/Article.cs
@@ -0,0 +1,35 @@
+﻿class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+
+    }
+}
+
