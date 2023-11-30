0db42bab15714f26296b6218684400a61f4a4349
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-12-01 00:39:51 +0200

    Task 1

---
+++ Programming Advanced for QA/13. Exception Handling/01. Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num < 0)
+    {
+        //throw new Exception();
+        throw new ArgumentException();
+
+    }
+    double sqrt = Math.Sqrt(num);
+
+    Console.WriteLine(sqrt);
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("Invalid number.");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid format.");
+}
+
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
