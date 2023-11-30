295af343bbef7fb90b74eae11c586894c8dc56e7
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-12-01 00:52:04 +0200

    Task 2

---
+++ Programming Advanced for QA/13. Exception Handling/02. Enter Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿static int ReadNumber(int start, int end)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num <= start || num >= end)
+    {
+        throw new ArgumentException();
+    }
+    return num;
+}
+List<int> numbers = new List<int>();
+int prevNum = 1;
+while (numbers.Count < 10)
+{
+    try
+    {
+        int number = ReadNumber(prevNum, 100);
+
+        numbers.Add(number);
+        prevNum = number;
+    }
+    catch (ArgumentException)
+    {
+        Console.WriteLine($"Your number is not in range {prevNum} - 100!");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+}
+Console.WriteLine(string.Join(", ", numbers));
