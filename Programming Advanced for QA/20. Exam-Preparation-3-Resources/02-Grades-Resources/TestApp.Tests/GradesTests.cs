d02e41b9e1f4da5f2776fa5e202af1d0209a7e2e
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-12-16 13:24:10 +0200

    Task 2

--- Programming Advanced for QA/20. Exam-Preparation-3-Resources/02-Grades-Resources/TestApp.Tests/GradesTests.cs
+++ Programming Advanced for QA/20. Exam-Preparation-3-Resources/02-Grades-Resources/TestApp.Tests/GradesTests.cs
@@ -11,24 +11,73 @@ public class GradesTests
     [Test]
     public void Test_GetBestStudents_ReturnsBestThreeStudents()
     {
-        // TODO: finish the test
+        Dictionary<string, int> gradesDict = new()
+        {
+            ["Gosho"] = 5,
+            ["Ivan"] = 3,
+            ["Petkan"] = 4,
+            ["Stoian"] = 6,
+            ["Pesho"] = 2
+        };
+
+        string results = Grades.GetBestStudents(gradesDict);
+        string expected = $"Stoian with average grade 6.00" +
+            $"{Environment.NewLine}Gosho with average grade 5.00" +
+            $"{Environment.NewLine}Petkan with average grade 4.00";
+
+        Assert.That(results, Is.EqualTo(expected));
+
     }
 
     [Test]
     public void Test_GetBestStudents_EmptyGrades_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        Dictionary<string, int> gradesDict = new()
+        {
+
+        };
+
+        string results = Grades.GetBestStudents(gradesDict);
+        string expected = "";
+
+        Assert.That(results, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetBestStudents_LessThanThreeStudents_ReturnsAllStudents()
     {
-        // TODO: finish the test
+        Dictionary<string, int> gradesDict = new()
+        {
+            ["Gosho"] = 5,
+            ["Ivan"] = 3,
+
+        };
+
+        string results = Grades.GetBestStudents(gradesDict);
+        string expected = $"Gosho with average grade 5.00" +
+            $"{Environment.NewLine}Ivan with average grade 3.00";
+
+        Assert.That(results, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetBestStudents_SameGrade_ReturnsInAlphabeticalOrder()
     {
-        // TODO: finish the test
+        Dictionary<string, int> gradesDict = new()
+        {
+            ["Gosho"] = 5,
+            ["Ivan"] = 5,
+            ["Petkan"] = 5,
+            ["Stoian"] = 5,
+            ["Pesho"] = 5
+
+        };
+
+        string results = Grades.GetBestStudents(gradesDict);
+        string expected = $"Gosho with average grade 5.00" +
+            $"{Environment.NewLine}Ivan with average grade 5.00" +
+            $"{Environment.NewLine}Pesho with average grade 5.00";
+
+        Assert.That(results, Is.EqualTo(expected));
     }
 }
