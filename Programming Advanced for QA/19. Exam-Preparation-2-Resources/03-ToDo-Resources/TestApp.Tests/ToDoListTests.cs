db40ae7881c9c6a93896b95fa002c5fb7ccc5ba6
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-12-16 12:22:36 +0200

    Task 3

--- Programming Advanced for QA/19. Exam-Preparation-2-Resources/03-ToDo-Resources/TestApp.Tests/ToDoListTests.cs
+++ Programming Advanced for QA/19. Exam-Preparation-2-Resources/03-ToDo-Resources/TestApp.Tests/ToDoListTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 using NUnit.Framework;
 
@@ -10,40 +10,68 @@ namespace TestApp.Tests;
 public class ToDoListTests
 {
     private ToDoList _toDoList = null!;
-    
+
     [SetUp]
     public void SetUp()
     {
         this._toDoList = new();
     }
-    
+
     [Test]
     public void Test_AddTask_TaskAddedToToDoList()
     {
-        // TODO: finish the test
+        string taskName = "my first task";
+        DateTime taskDate = DateTime.Today; // another options DateTime.Now - with hour and seconds though
+        this._toDoList.AddTask(taskName, taskDate);
+
+        string result = this._toDoList.DisplayTasks();
+
+        Assert.That(result, Does.Contain("[ ] my first task - Due:"));
     }
 
     [Test]
     public void Test_CompleteTask_TaskMarkedAsCompleted()
     {
-        // TODO: finish the test
+        string taskName = "task to complete";
+        DateTime taskDate = DateTime.Today;
+        this._toDoList.AddTask(taskName, taskDate);
+        this._toDoList.CompleteTask(taskName);
+
+        string result = this._toDoList.DisplayTasks();
+
+        Assert.That(result, Does.Contain("[✓] task to complete - Due:"));
     }
 
     [Test]
     public void Test_CompleteTask_TaskNotFound_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        string taskName = "task to complete";
+        
+        Assert.That(() => this._toDoList.CompleteTask(taskName), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_DisplayTasks_NoTasks_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        string result = this._toDoList.DisplayTasks();
+
+        Assert.That(result,Is.EqualTo("To-Do List:"));
     }
 
     [Test]
     public void Test_DisplayTasks_WithTasks_ReturnsFormattedToDoList()
     {
-        // TODO: finish the test
+        string taskName = "my first task";
+        DateTime taskDate = DateTime.Today;
+        this._toDoList.AddTask(taskName, taskDate);
+        string taskName2 = "task to complete";
+        DateTime taskDate2 = DateTime.Today;
+        this._toDoList.AddTask(taskName2, taskDate2);
+        this._toDoList.CompleteTask(taskName2);
+
+        string result = this._toDoList.DisplayTasks();
+
+        Assert.That(result, Does.Contain("[ ] my first task - Due:"));
+        Assert.That(result, Does.Contain("[✓] task to complete - Due:"));
     }
 }
