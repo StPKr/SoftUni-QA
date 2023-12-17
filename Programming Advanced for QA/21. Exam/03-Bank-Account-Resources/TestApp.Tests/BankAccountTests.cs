1720b5625d1ba99bc3ad33a414f62df8b3d97c5f
Public Name <137268124+Codedelayer@users.noreply.github.com>
2023-12-17 10:00:25 +0200

    Task 3

--- Programming Advanced for QA/21. Exam/03-Bank-Account-Resources/TestApp.Tests/BankAccountTests.cs
+++ Programming Advanced for QA/21. Exam/03-Bank-Account-Resources/TestApp.Tests/BankAccountTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TestApp.Tests;
 
@@ -8,36 +10,67 @@ public class BankAccountTests
     [Test]
     public void Test_Constructor_InitialBalanceIsSet()
     {
-        // TODO: finish the test
+        double input = 200;
+
+        BankAccount bankAccount = new BankAccount(input);
+
+        Assert.AreEqual(input, bankAccount.Balance);
+
     }
 
     [Test]
     public void Test_Deposit_PositiveAmount_IncreasesBalance()
     {
-        // TODO: finish the test
+        BankAccount bankAccount = new BankAccount(200);
+        bankAccount.Deposit(50);
+
+        Assert.AreEqual(250, bankAccount.Balance);
     }
 
     [Test]
     public void Test_Deposit_NegativeAmount_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        double input = 200;
+        BankAccount bankAccount = new BankAccount(input);
+
+        double deposit = -50;
+
+        Assert.That(() => bankAccount.Deposit(deposit), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_Withdraw_ValidAmount_DecreasesBalance()
     {
-        // TODO: finish the test
+        double input = 200;
+        BankAccount bankAccount = new BankAccount(input);
+
+        double withdrawal = 50;
+        bankAccount.Withdraw(withdrawal);
+
+        double expected = 150;
+
+        Assert.AreEqual(expected, bankAccount.Balance);
     }
 
     [Test]
     public void Test_Withdraw_NegativeAmount_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        double input = 200;
+        BankAccount bankAccount = new BankAccount(input);
+
+        double withdrawal = -50;
+
+        Assert.That(() => bankAccount.Withdraw(withdrawal), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_Withdraw_AmountGreaterThanBalance_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        double input = 200;
+        BankAccount bankAccount = new BankAccount(input);
+
+        double withdrawal = 250;
+
+        Assert.That(() => bankAccount.Withdraw(withdrawal), Throws.ArgumentException);
     }
 }
